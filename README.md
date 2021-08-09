010ee9cee4dbfcb85510811b723db3cd06c2baee
Remy Ochei <remy@bodkin.lan>
2021-08-09 15:48:39 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+https://www.reddit.com/r/copypasta/comments/9vqalp/owo_imagine_reading_a_post_uwu/
---
+++ model.py
@@ -0,0 +1,10 @@
+
+class UwuFilter():
+    def __init__(self, latent_size):
+        self.latent_size
+        self.resample()
+    def forward(self, x):
+        self.phoneme_quality_weight_vector = x
+        pass
+    def resample(self):
+        self.latent_noise = 
